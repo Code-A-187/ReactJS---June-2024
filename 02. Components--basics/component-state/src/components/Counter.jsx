57601e6349d060d512fd474a37a42ac62cdf6ae9
Anton Simeonov <aplamenov@gmail.com>
2024-07-06 17:30:25 +0300

    state hook demo

---
+++ 02. Components--basics/component-state/src/components/Counter.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function Counter () {
+
+    const [count, setCount] = useState(0)  // Read only
+    
+    // console.log(stateArr)
+    // let count = stateArr[0]
+    // let setCount = stateArr[1]
+
+    setTimeout(() => {
+        setCount(count + 1)
+    }, 1000);
+
+
+    return (
+        <>
+            <h2>Counter</h2>
+
+            <p>{count}</p>
+        </>
+    )
+}
