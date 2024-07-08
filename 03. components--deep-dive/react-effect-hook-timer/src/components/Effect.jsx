da85010d91596cd2ccf3910f01541fe834d267af
Anton Simeonov <aplamenov@gmail.com>
2024-07-08 14:36:12 +0300

    Timer with effect

---
+++ 03. components--deep-dive/react-effect-hook-timer/src/components/Effect.jsx
@@ -0,0 +1,5 @@
+export default function Effect() {
+    return (
+        <h2>Effect</h2>
+    )
+}
---
+++ 03. components--deep-dive/react-effect-hook-timer/src/components/Timer.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+export default function Timer() {
+    const [isManual, setIsManual] = useState(false)
+    const [time, setTime] = useState(() => {
+        const currentSeconds = new Date().getSeconds();
+        console.log('Get seconds');
+
+        return currentSeconds
+    });
+    
+    
+    useEffect(() => {
+        if (!isManual) {setTimeout(() => {
+            setTime(prevTime => prevTime + 1)
+            setIsManual(false);
+        }, 1000);
+        }
+    }, [time, isManual]);
+
+    const addSeocondsHandler = () => {
+        setIsManual(true);
+        setTime(prevTime => prevTime + 10)
+    };
+
+    return (
+        <>
+            <h1>Timer</h1>
+            <div>{time}</div>
+            <button onClick={addSeocondsHandler}>Add Seconds</button>
+        </>
+    )
+}
