8f7540892ad45b947ff4145e83611ce86a9ebe63
Anton Simeonov <aplamenov@gmail.com>
2024-07-12 09:25:41 +0300

    react uncontrolled forms demo

--- 05. React-JS-forms-ref--hook/src/components/UncontrolledForm.jsx
+++ 05. React-JS-forms-ref--hook/src/components/UncontrolledForm.jsx
@@ -1,24 +1,44 @@
+import { useState } from "react";
+
 export default function UncontrolledForm() {
-  return (
-       <>
+    const [user, setUser] = useState({})
+    
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        
+        const formData = new FormData(e.target);
+        
+        setUser({
+            username: formData.get('username'),
+        })
+    }
+
+    const logoutHandler = () => {
+        setUser({})
+    }
+    return (
+        <>  
             <h1>Uncotrolled Form</h1>
+            {user.username
+                ? <p>Hello, {user.username}! <button onClick={logoutHandler}>Logout</button></p>
+                : (
+                    <form onSubmit={formSubmitHandler}>
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name='username' id='username'/>
+                    </div>
+    
+                    <div>
+                        <label htmlFor="passowrd">Password</label>
+                        <input type="password" name='password' id="password" />
+                    </div>
+    
+                    <div>
+                        <input type="submit" value='Login'/>
+                    </div>
+                </form>
 
-            <form>
-                <div>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name='username' id='username'/>
-                </div>
-
-                <div>
-                    <label htmlFor="passowrd">Password</label>
-                    <input type="password" name='password' id="password" />
-                </div>
-
-                <div>
-                    <input type="submit" value='Login'/>
-                </div>
-            </form>
-
-       </>
+            )}
+        </>
    );
 }
