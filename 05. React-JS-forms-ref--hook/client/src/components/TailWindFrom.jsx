854075711b784248cd85a040610bf3ca845852a8
Anton Simeonov <aplamenov@gmail.com>
2024-07-12 17:02:08 +0300

    Update TailWindFrom.jsx

--- 05. React-JS-forms-ref--hook/client/src/components/TailWindFrom.jsx
+++ 05. React-JS-forms-ref--hook/client/src/components/TailWindFrom.jsx
@@ -40,7 +40,7 @@ export default forwardRef(function TailWindForm(props, ref) {
                     onChange={changeHandler}
                     required
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                 </div>
             </div>
@@ -65,7 +65,7 @@ export default forwardRef(function TailWindForm(props, ref) {
                     onChange={changeHandler}
                     required
                     autoComplete="current-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                 </div>
             </div>
