36a398fe70c2dde80aae844174ab78dbf3882dc4
Anton Simeonov <aplamenov@gmail.com>
2024-07-14 15:24:54 +0300

    abort controller demo

--- 06. React-JS-Routing/client/src/components/Articles.jsx
+++ 06. React-JS-Routing/client/src/components/Articles.jsx
@@ -5,13 +5,23 @@ import { Link } from "react-router-dom";
     const [articles, setArticles] = useState([]);
 
     useEffect(() =>{
+      const abortController = new AbortController();
+
       (async () => {
-        const response = await fetch('http://localhost:3030/jsonstore/advanced/articles/list')
+        const response = await fetch('http://localhost:3030/jsonstore/advanced/articles/list', {
+            signal: abortController.signal
+          });
+            
         const result = await response.json()
 
-        setArticles(result)
+        setArticles(result);
 
       })();
+
+      return () => {
+        abortController.abort
+
+      }
 
     }, [])
     return (
