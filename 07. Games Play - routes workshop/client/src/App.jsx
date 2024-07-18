b54d6af7cff08262cb18f9ab041e65ade7379653
Anton Simeonov <aplamenov@gmail.com>
2024-07-18 10:56:21 +0300

    separate html to components and convert html to jsx

--- 07. Games Play - routes workshop/client/src/App.jsx
+++ 07. Games Play - routes workshop/client/src/App.jsx
@@ -1,9 +1,29 @@
+import AllGames from "./components/all-games/AllGames"
+import CreateGame from "./components/create-game/CreateGame"
+import Header from "./components/Header"
+import Home from "./components/home/Home"
+import UserLogin from "./components/user-login/UserLogin"
+import UserRegister from "./components/user-register/UserRegister"
+
 function App() {
 
   return (
-    <>
+    <div id="box">
+
+      <Header />
+
+      <main id="main-content">
+          <Home />
+
+          <AllGames/>
+          <CreateGame />
+          <UserLogin />
+          <UserRegister />
+          
+          
+      </main>
       
-    </>
+    </div>
   )
 }
 
